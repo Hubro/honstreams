dea431a6f4dd5293efd3fc4eb3783e2651d76a5a
Tomas Sandven <tomas191191@gmail.com>
2012-04-20 00:07:56 +0200

    Removed check scripts. All my work is done on linux anyway now, UTF-8 is default as well as LF line endings

--- check-charsets.sh
+++
@@ -1,1 +0,0 @@
-tree -f -i -a -I node_modules --noreport -n | xargs file -i | grep -v "directory" | grep -v ".git"
--- check-lineterminators.sh
+++
@@ -1,1 +0,0 @@
-tree -f -i -a -I node_modules --noreport -n | xargs file | grep -v directory | grep "line terminators"
